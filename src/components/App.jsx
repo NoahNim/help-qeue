d93ed710d4a5e63d6b8f49e4ada4901faf9378a7
Noah Medoff and Randy Weyer <noah@>
2018-09-04 13:19:39 -0700

    sdd eslinter to test code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 
-import React from "react";
-import Header from "./Header";
-import TicketList from "./TicketList";
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
 
 function App(){
   return (
